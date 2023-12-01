3a4d6ec7acff5112747f6aca4d7bf9fe62cbd37a
Damon-Galeya <nmadayag@bu.edu>
2023-11-30 20:36:44 -0500

    Implemented Functions in Library.cpp

--- Lab8code/Library.cpp
+++ Lab8code/Library.cpp
@@ -1,3 +1,38 @@
 //include headers and libraries
-
+#include "Library.h"
 //implement class functions
+bool Library::addBook(string bookName){
+    //check if the book already exists
+    for(int i = 0; i < 10; i++){
+        if(books[i] == bookName){
+            return false;
+        }
+    }
+    //check if the library is full
+    for(int i = 0; i < 10; i++){
+        if(books[i] == ""){
+            books[i] = bookName;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Library::removeBook(string bookName){
+    //check if the book exists
+    for(int i = 0; i < 10; i++){
+        if(books[i] == bookName){
+            books[i] = "";
+            return true;
+        }
+    }
+    return false;
+}
+
+void Library::print(){
+    for(int i = 0; i < 10; i++){
+        if(books[i] != ""){
+            cout << books[i] << endl;
+        }
+    }
+}
